eb8ac66e70d28b16edcda6acc2adb992589d8d8c
tienle2003 <minhtienbangbang2003@gmail.com>
2023-05-10 15:31:22 +0700

    update animate, font and button

---
+++ js/HomePage/ModelSlider.js
@@ -0,0 +1,87 @@
+const slider2Forward = () => {
+    const slides = document.querySelectorAll('.slide2');
+    const slideActive = slides[2];
+    const copyOfActiveSlide = slides[2].cloneNode(true);
+    const slideContainer = document.querySelector('.slides2');
+
+    slideContainer.insertBefore(slides[2], slideContainer.children[0]);
+    slideContainer.appendChild(copyOfActiveSlide);
+    slideActive.classList.remove('active');
+
+    setTimeout(() => {
+        slides[1].classList.add('active')
+        copyOfActiveSlide.style.width = '0';
+
+        setTimeout(() => {
+            copyOfActiveSlide.remove();
+            // console.log(slides);
+        }, 501)
+    }, 1)
+}
+
+const slider2Back = () => {
+    const slides = document.querySelectorAll('.slide2');
+    const slideActive = slides[0];
+    const slideActiving = slides[2];
+    const copyOfActiveSlide = slides[0].cloneNode(true);
+    const slideContainer = document.querySelector('.slides2');
+
+    copyOfActiveSlide.style.transitionDuration = '0';
+    copyOfActiveSlide.style.width = '0';
+    slideContainer.appendChild(copyOfActiveSlide);
+
+    setTimeout(() => {
+        copyOfActiveSlide.removeAttribute('style');
+
+        setTimeout(() => {
+            copyOfActiveSlide.classList.add('active');
+            slideActiving.classList.remove('active');
+
+            setTimeout(() => {
+                slideActive.remove();
+            }, 503);
+        }, 1);
+    }, 1)
+}
+
+const sloganCar = () => {
+    const carActive = document.querySelector('.slide2.active').getAttribute("data-index");
+    const slogan = document.querySelector(`.slide2-text-container .slogan${carActive}`);
+    
+    slogan.classList.add('active');
+}
+
+const logicForModelSlider = () => {
+    const navBtns = document.querySelectorAll('.slider2-navigation-btn .btn')
+    navBtns.forEach(btn => {
+        let check = true;
+        btn.onclick = e => {
+            e.stopPropagation();
+            while (check == true) {
+                check = false;
+                const sloganActive = document.querySelector(`.slide2-text-container .active`);
+                sloganActive?.classList?.remove('active');
+                if (btn?.classList?.contains('btn-left')) {
+                    slider2Back();
+                } else {
+                    slider2Forward();
+                }
+                setTimeout(() => {
+                    check = true;
+                    sloganCar();
+                }, 650);
+            }
+        }
+    });
+
+    //Create
+    const navCreateBtns = document.querySelectorAll('.content-create-nav__choose-name')
+    navCreateBtns.forEach(btnCreate => {
+        btnCreate.onclick = e => {
+            e.stopPropagation();
+            navCreate(btnCreate)
+        }
+    })
+}
+
+logicForModelSlider();
---
+++ js/HomePage/VideoSlider.js
@@ -0,0 +1,33 @@
+const videoSlider = document.querySelector(".video__slider");
+const videoSliderContent = document.querySelector(".video__slider--content");
+const videoSliderBtn = document.querySelector(".video__slider--btn");
+
+function active() {
+  videoSlider.classList.add("active");
+  videoSliderContent.classList.add("active");
+  videoSliderBtn.classList.add("active");
+  displayEachWord("60th Anniversary", ".video__slider--title", 30);
+}
+
+window.addEventListener("load", active);
+
+function displayEachWord(text, element, delay) {
+  const wordsStorage = document.querySelector(element);
+  const words = text.split("");
+  const lenght = words.length;
+  let i = 0;
+  setInterval(function () {
+    if (i === lenght) {
+      clearInterval(this);
+      return;
+    }
+    const wordElement = document.createElement("span");
+    wordElement.classList.add("word");
+    wordElement.innerHTML = words[i];
+    wordsStorage.appendChild(wordElement);
+    i++;
+    setTimeout(function () {
+      wordElement.classList.add("active");
+    }, 50);
+  }, delay);
+}
