32610bd75f92eb3b5d6ca19898f01078ed234b72
Dinh Tuan Dat <dtuandat2004@gmail.com>
2023-05-20 16:31:14 +0700

    update header

--- js/common/Header.js
+++ js/common/Header.js
@@ -2,7 +2,7 @@ let prevScrollpos = window.pageYOffset;
 window.onscroll = function () {
   let currentScrollPos = window.pageYOffset;
   if (prevScrollpos > currentScrollPos) {
-    document.querySelector('.header').style.top = "86px";
+    document.querySelector('.header').style.top = "84px";
   } else {
     document.querySelector('.header').style.top = "-100px";
   }
