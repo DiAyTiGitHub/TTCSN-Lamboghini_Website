e81e3c67e43fb90b940e346da1e8ef1fe552b196
Mona Lisa <1pho2trung@gmail.com>
2023-05-25 17:31:47 +0700

    add highlight, cmt end slider

--- js/Revuelto/HighlightSlider.js
+++ js/Revuelto/HighlightSlider.js
@@ -1,7 +1,32 @@
+var carouselWidth = $(".tablet-highlight .carousel-inner")[0].scrollWidth;
+var cardWidth = $(".tablet-highlight .carousel-item").width();
+var scrollPosition = 0;
+
+$(".tablet-highlight .carousel-control-next").on("click", function () {
+  if (scrollPosition < (carouselWidth - cardWidth * 6)) { //check if you can go any further
+    scrollPosition += cardWidth;  //update scroll position
+    console.log('1');
+    $(".tablet-highlight .carousel-inner").animate({ scrollLeft: scrollPosition },600); //scroll left
+  }
+});
+
+$(".tablet-highlight .carousel-control-prev").on("click", function () {
+  if (scrollPosition > 0) {
+    scrollPosition -= cardWidth;
+    console.log('2');
+    $(".tablet-highlight .carousel-inner").animate(
+      { scrollLeft: scrollPosition },
+      600
+    );
+  }
+  
+
+});
 
 $('#nav-slider-next, #nav-slider-prev').on('click', function (e) {
-    var inst = $(this).data("slide");
-    if(inst== "prev"){ 
-    $("#highlightTextControls").carousel("prev")}
-    else{ $("#highlightTextControls").carousel("next")}
-  })
+  var inst = $(this).data("slide");
+  if(inst== "prev"){ 
+  $("#highlightTextControls").carousel("prev")}
+  else{ $("#highlightTextControls").carousel("next")}
+})
+console.log('2');
