5570e1b1fc59a0d8c41cc06f0d9a9ce2141a361f
TrDHuy21 <vlsvhuy@gmail.com>
2023-06-05 15:00:03 +0700

    huy fix header

--- js/common/Huy_header.js
+++ js/common/Huy_header.js
@@ -220,4 +220,15 @@ const header = () => {
 
 }
 
+let prevScrollpos = window.pageYOffset;
+window.onscroll = function () {
+    let currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+        document.getElementById('header').style.display = "block";
+    } else {
+        document.getElementById('header').style.display = "none";
+    }
+    prevScrollpos = currentScrollPos;
+}
+
 header();
